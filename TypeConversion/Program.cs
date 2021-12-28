6e454a7dbcfc4fb984a962fb0dc41e66729cc176
christopher.m.alvarez93@gmail.com <christopher.m.alvarez93@gmail.com>
2021-12-28 12:13:51 -0600

    Type Conversion

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // byte b = 1;
+            // int i = b;
+            // Console.WriteLine(i);
+
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+        }
+    }
+}
