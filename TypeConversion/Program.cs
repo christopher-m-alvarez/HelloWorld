6ca48151c2b6eb2756a39bd0d9c0327e0cfd1c13
christopher.m.alvarez93@gmail.com <christopher.m.alvarez93@gmail.com>
2022-01-05 14:30:27 -0600

    Try catch examples

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -14,9 +14,30 @@ namespace TypeConversion
             // int i = b;
             // Console.WriteLine(i);
 
-            var number = "1234";
-            int i = Convert.ToInt32(number);
-            Console.WriteLine(i);
+            // var number = "1234";
+            // int i = Convert.ToInt32(number);
+            // Console.WriteLine(i);
+            //try
+            //{
+            //    var number = "1234";
+            //    byte b = Convert.ToByte(number);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte");
+            //}
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+
         }
     }
 }
