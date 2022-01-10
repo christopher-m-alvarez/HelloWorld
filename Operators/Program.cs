48baf9a863d98b7fabca107b31a32b76e9813528
christopher.m.alvarez93@gmail.com <christopher.m.alvarez93@gmail.com>
2022-01-10 15:11:10 -0600

    operators

---
+++ Operators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(c > b || c > a);
+
+            // Console.WriteLine(c > b && c > a);
+
+            // var a = 1;
+            // var b = 2;
+            // 
+            // Console.WriteLine(a < b);
+
+
+            //var a = 1;
+            //var b = 2;
+            //var c = 3;
+            //
+            //Console.WriteLine((a + b) * c);
+
+
+            //var a = 10;
+            //var b = 3;
+            //
+            //Console.WriteLine(a + b);
+            //Console.WriteLine((float)a / (float)b);
+        }
+    }
+}
--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -27,16 +27,16 @@ namespace TypeConversion
             //{
             //    Console.WriteLine("The number could not be converted to a byte");
             //}
-            try
-            {
-                string str = "true";
-                bool b = Convert.ToBoolean(str);
-                Console.WriteLine(b);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("The number could not be converted to a byte");
-            }
+            // try
+            // {
+            //     string str = "true";
+            //     bool b = Convert.ToBoolean(str);
+            //     Console.WriteLine(b);
+            // }
+            // catch (Exception)
+            // {
+            //     Console.WriteLine("The number could not be converted to a byte");
+            // }
 
         }
     }
