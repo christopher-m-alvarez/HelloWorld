11693b1e69f8880d0bed604111824f35c843ea8d
christopher.m.alvarez93@gmail.com <christopher.m.alvarez93@gmail.com>
2022-04-05 14:53:54 -0500

    Random Classes

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                }
+
+                break;
+            }
+        }
+    }
+}
