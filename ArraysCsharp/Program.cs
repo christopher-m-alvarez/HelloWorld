39ba65a0c82b055ebe10843b958b36acd8a0e132
christopher.m.alvarez93@gmail.com <christopher.m.alvarez93@gmail.com>
2022-04-06 12:55:29 -0500

    Array methods

---
+++ ArraysCsharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            //Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+
+        }
+    }
+}
